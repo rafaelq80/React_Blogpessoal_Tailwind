6e4fc17ead63bc9c70249dd4614651766cd5317a
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-05-18 00:27:56 -0300

    Consumir Listar Postagens

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -1,21 +1,27 @@
 ﻿import { Link } from "react-router-dom"
+import Postagem from "../../../models/Postagem"
 
-function CardPostagem() {
+interface CardPostagemProps {
+  post: Postagem
+}
+
+function CardPostagem({post}: CardPostagemProps) {
+
     return (
       <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
         <div>
           <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
-            <img src='https://i.imgur.com/Zz4rzVR.png' className='h-12 rounded-full' alt="" />
-            <h3 className='text-lg font-bold text-center uppercase '>Usuário - Nome</h3>
+            <img src={post.usuario?.foto} className='h-12 rounded-full' alt="" />
+            <h3 className='text-lg font-bold text-center uppercase '>{post.usuario?.nome}</h3>
           </div>
           <div className='p-4 '>
-            <h4 className='text-lg font-semibold uppercase'>Título</h4>
-            <p>Texto</p>
-            <p>Tema: Tema - Descrição</p>
+            <h4 className='text-lg font-semibold uppercase'>{post.titulo}</h4>
+            <p>{post.texto}</p>
+            <p>Tema: {post.tema?.descricao}</p>
             <p>Data: {new Intl.DateTimeFormat('pt-br', {
                       dateStyle: 'full',
                       timeStyle: 'medium',
-                    }).format(new Date())}</p>
+                    }).format(new Date(post.data))}</p>
           </div>
         </div>
         <div className="flex">
--- src/components/postagens/listarpostagens/ListarPostagens.tsx
+++ src/components/postagens/listarpostagens/ListarPostagens.tsx
@@ -1,11 +1,42 @@
-﻿import CardPostagem from '../cardpostagens/CardPostagens';
+﻿import useLocalStorage from 'react-use-localstorage';
+import { listar } from '../../../services/Service';
+import CardPostagem from '../cardpostagens/CardPostagens';
+import Postagem from '../../../models/Postagem';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function ListarPostagens() {
+
+  const [token, setToken] = useLocalStorage('token');
+  const [postagens, setPostagens] = useState<Postagem[]>([]);
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (token === '') {
+      alert('Você precisa estar logado');
+      navigate('/login');
+    }
+  }, [token]);
+
+  async function buscarPostagens() {
+    await listar('/postagens', setPostagens, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    buscarPostagens();
+  }, [postagens.length]);
 
   return (
     <>
       <div className='container mx-auto my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-          <CardPostagem />
+        {postagens.map((postagem) => (
+          <CardPostagem key={postagem.id} post={postagem} />
+        ))}
       </div>
     </>
   );
