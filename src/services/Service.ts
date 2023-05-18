e1c2784a11390949c4b80f2ed6908161d8308ea7
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-05-17 21:18:45 -0300

    Criar a Classe de Serviço e Model UsuarioLogin

---
+++ src/services/Service.ts
@@ -0,0 +1,10 @@
+﻿import axios from "axios"
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080/'
+  })
+  
+  export const login = async(url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+  }
