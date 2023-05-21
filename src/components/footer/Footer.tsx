b8306892d42a54fde9f0ef8a319795bd2337997b
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-05-21 05:55:58 -0300

    Renderização Condicional

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,38 +1,42 @@
 ﻿import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
+    const { usuario, handleLogout } = useContext(AuthContext)
+
+    let footerComponent
+
     let data = new Date().getFullYear()
+
+    if (usuario.token !== '') {
+        footerComponent = (
+            <>
+                <div className="flex justify-center bg-indigo-900 text-white">
+                    <div className="container flex flex-col items-center py-4">
+                        <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
+                        <p className='text-lg'>Acesse nossas redes sociais</p>
+                        <div className='flex gap-2'>
+                            <a href="https://www.linkedin.com/school/generationbrasil" target="_blank">
+                                <LinkedinLogo size={48} weight='bold' />
+                            </a>
+                            <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                                <InstagramLogo size={48} weight='bold' />
+                            </a>
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                <FacebookLogo size={48} weight='bold' />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
 
     return (
         <>
-            <div className="
-                flex 
-                justify-center
-                bg-indigo-900
-                text-white
-                ">
-                <div className="
-                    container 
-                    flex 
-                    flex-col 
-                    items-center 
-                    py-4
-                    ">
-                    <p className='
-                        text-xl 
-                        font-bold
-                        '>
-                            Blog pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
-                    </div>
-                </div>
-            </div>
+            {footerComponent}
         </>
     )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,9 @@
-﻿import { useContext } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
+﻿import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Navbar() {
-
-    let navigate = useNavigate()
+  let navigate = useNavigate()
 
     const { usuario, handleLogout } = useContext(AuthContext)
 
@@ -14,35 +13,31 @@ function Navbar() {
         navigate('/login')
     }
 
-    return (
-        <>
-            <div className='
-                w-full 
-                bg-indigo-900
-                text-white
-                flex 
-                justify-center 
-                py-4
-                '>
-                <div className="
-                    container 
-                    flex 
-                    justify-between 
-                    text-lg
-                ">
-                    <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+    let navbarComponent
 
-                    <div className='flex gap-4'>
-                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
-                        <Link to='/temas' className='hover:underline'>Temas</Link>
-                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-                        Perfil
-                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-                    </div>
-                </div>
+    if(usuario.token !== "") {
+      navbarComponent = (
+        <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+            <div className='flex gap-4'>
+              <Link to='/postagens' className='hover:underline'>Postagens</Link>
+              <Link to='/temas' className='hover:underline'>Temas</Link>
+              <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+              Perfil
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      )
+    }
+
+  return (
+    <>
+      {navbarComponent}
+    </>
+  )
 }
 
 export default Navbar
